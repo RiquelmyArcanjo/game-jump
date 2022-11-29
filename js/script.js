415958f8de8dde0b578051560f94c42d816f76d2
Riquelmy Arcanjo de Araújo <riquelmyaaraujo@gmail.com>
2022-11-28 22:28:35 -0300

    Novo avatar, obstáculo, inclusão de audios e timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 const avatar = document.querySelector('.avatar');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const gameover = './images/game-over.gif';
+let segundos = 0;
+let minutos = 0;
+
+function numeroInteiroRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function segundo(){
+    //incrementa os segundos
+    segundos++;
+    if(segundos==60){
+        //incrementa os minutos
+        minutos++;
+        //Zerar os segundos
+        segundos=0;
+        //escreve os minutos
+        document.getElementById('minuto').innerHTML=minutos
+    }
+    //escreve os segundos
+    document.getElementById('segundo').innerHTML=segundos
+    
+}
+
+const cronometro = setInterval(function(){ segundo() },1000)
+
 
 const jump = () => {
     avatar.classList.add('jump');
@@ -15,34 +43,44 @@ const jump = () => {
             avatar.classList.remove('jump');
         }, 500)
     }
-
+    
+    document.getElementById('musica-fundo').src = 'audios/jump/'+ numeroInteiroRandom(0, 15) + '.mp3';
+    document.getElementById('musica-fundo').play();
 }
 
+//Jogador perde
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const avatarPosition = +window.getComputedStyle(avatar).bottom.replace('px', '');
     const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px', '');
-    console.log(avatarPosition);
 
     if ((pipePosition < 120 || pipePosition == 120) && pipePosition > 0  && avatarPosition < 100) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         avatar.style.animation = 'none';
-        avatar.style.bottom = `${avatarPosition}px`;
+        //avatar.style.bottom = `${avatarPosition}px`;
 
-        avatar.src = './images/game-over.png';
-        avatar.style.width = '75px';
-        avatar.style.marginLeft = '50px';
+        avatar.src = gameover;
+        avatar.style.width = '350px';
+        avatar.style.marginLeft = '700px';
 
         clouds.style.animation = 'none';
         clouds.style.right = `${cloudsPosition}px`;
         
-        clearInterval(loop); // quando acaba o jogo o loop para de rodar
+        //quando acaba o jogo o loop para de rodar
+        clearInterval(loop); 
+        clearInterval(cronometro);
+
+        document.getElementById('musica-fundo').volume = 0.0;
+        document.getElementById('audio-game-over').src = 'audios/game-over/'+ numeroInteiroRandom(0, 5) + '.mp3';
+        document.getElementById('audio-game-over').play();
     }
     
 }, 10);
 
 document.addEventListener('mousedown', jump);
 document.addEventListener('keydown', jump);
+
+
